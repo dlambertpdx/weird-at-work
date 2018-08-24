4f281c4ecc353b36596dd197f534f1565346b2de
Donna Lambert <dlambertpdx@gmail.com>
2018-08-24 13:39:36 -0700

    add users and posts routes

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+"use strict";
+
+module.exports = {
+  mongoURI: "mongodb://donna:donna123@ds121674.mlab.com:21674/waw-dev"
+};
--- server.js
+++ server.js
@@ -1,8 +1,40 @@
+"use strict";
+
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
-app.use(express.static("public"));
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+// BodyParser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//app.use(express.static("public"));
+
+//Use Routes
+app.use("/api/users", users);
+app.use("/api/posts", posts);
 
 if (require.main === module) {
   app.listen(process.env.PORT || 8080, function() {
