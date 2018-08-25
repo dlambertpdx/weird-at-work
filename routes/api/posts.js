c55c05a8c1a810305b1a485568af9fb755cd9dfd
Donna Lambert <dlambertpdx@gmail.com>
2018-08-25 16:41:44 -0700

    add delete route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,12 +4,27 @@ const mongoose = require("mongoose");
 const passport = require("passport");
 
 const Post = require("../../models/Post");
+const User = require("../../models/User");
 
-// @route   GET api/posts/test
-// @desc    Tests posts route
+// @route   GET api/posts
+// @desc    Get posts
 // @access  Public
-router.get("/test", (req, res) => {
-  res.json({ message: "Posts works" });
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ noposts: "No posts found" }));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get posts by ID
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopost: "No post found with that ID" })
+    );
 });
 
 // @route   POST api/posts
@@ -27,6 +42,50 @@ router.post(
     });
 
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post
+// @access  Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    User.findOne({ user: req.user.id }).then(user => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // Check for post owner
+          if (post.user.toString() !== req.user.id) {
+            res.status(401).json({ notauthorized: "User not authorized" });
+          }
+          // Delete
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
+  }
+);
+
+// @route   POST api/posts/like/:id
+// @desc    Like post
+// @access  Private
+router.post(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    User.findOne({ user: req.user.id }).then(user => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // Check for post owner
+          if (post.user.toString() !== req.user.id) {
+            res.status(401).json({ notauthorized: "User not authorized" });
+          }
+          // Delete
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
   }
 );
 
